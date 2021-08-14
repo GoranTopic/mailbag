c78ca34634e5981b36fdbb0106adc1971608fc44
Goran Topic <telegonicaxx@live.com>
2021-08-14 16:06:26 -0700

    added some more api endpoints

--- dist/main.ts
+++ dist/main.ts
@@ -54,7 +54,7 @@ app.get("/mailboxes/:mailbox",
 
 // API Endpoint: get a message
 app.get("/messages/:mailbox/:id", 
-		async ( inReuqest: Request, inResponse: Response ) => {
+		async ( inRequest: Request, inResponse: Response ) => {
 				try{
 						// initialiaze and new imapworker with the sever info
 						const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
@@ -68,6 +68,74 @@ app.get("/messages/:mailbox/:id",
 				}catch( inError ){  
 						inResponse.send("error");
 				}
+		}
+);
+
+// API Endpoint: Delete a mesage
+app.delete("/messages/:mailbox/:id",  
+		async (inRequest: Request, inResponse: Response) => { 
+				try{
+						// initialiaze and new imapworker with the sever info
+						const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+						await imapWorker.deleteMessage({
+								mailbox: inRequest.params.mailbox,
+								id: parseInt(inRequest.params.id, 10)
+						});
+						// send a success response
+						inResponse.send("ok");
+				}catch( inError ){  
+						inResponse.send("error");
+				}
+		}
+);
+
+
+// API Endpoint: send a message
+app.post("/messages", 
+		async (inRequest: Request, inResponse: Response) => {
+				try{
+						// make a smtp worker 
+						const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
+						// send the body of the of the request 
+						await smtpWorker.sendMessage(inRequest.body);
+						// sedn success response
+						inResponse.send("ok");
+				}catch( inError ){
+						inResponse.send("error");
+				}
+		}
+);
+
+// API Endpoint: List Contacts
+app.get("/contacts", 
+		async (inRequest: Request, inResponse: Response) => {
+				try{
+						// initialize the nessary worker to get the contacts
+						const contactsWorker: Contacts.Worker = new Contacts.Worker()
+						// make the contacts worker fetch all the contacts
+						const contacts: IContact[] = await contactsWorker.listContacts();
+						// jsonify and send contacts
+						inResponse.json(contacts);
+				}catch( inError ){
+						inResponse.send("error");
+				}
+		}
+);
+
+// API Endpoint: add Contacts 
+app.post("/contacts", 
+		async (inRequest: Request, inResponse: Response) => {
+				try{
+						// initialize the nessary worker to get the contacts
+						const contactWorker: Contacts.Worker = new Contacts.Worker();
+						// use pass the body of the request to the contactworker 
+						const contact: IContact = await contactsWorker.addContact(inRequest.body);
+						// json the contact, but it send it?
+						inRequest.json(contact);
+				}catch( inError ){
+						inResponse.send("error");
+				}
+
 		}
 );
 
